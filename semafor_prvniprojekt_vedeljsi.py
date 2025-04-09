dfffa411dda3c7ebc65788cc10b27285747e1ed3
Dominik <mikuleckydominik@gmail.com>
2025-04-09 20:15:56 +0200

    dodelany aplikace srdíčko, oprava prechodu a uvodniho textu

--- semafor_prvniprojekt_vedeljsi.py
+++ semafor_prvniprojekt_vedeljsi.py
@@ -3,25 +3,32 @@ from tkinter import *
 okno = Tk()
 okno.geometry("500x1000+1200+0")
 okno.config(bg = "black")
-frame1 = None
+frame = None
 frame2 = None
 frame3 = None
+frame4 = None
+frame5 = None
+
+
+
+
 obrazek = PhotoImage(file = "C:\\Users\\mikul\\OneDrive\\Plocha\\python\\Projekt_Kalkulačka\\Python_calc\\logo2.png")
 
 def Semafor():
-    global frame , frame2, frame3
-    # if frame3:
-    #     frame3.destroy()
+    global frame , frame2, frame3, frame4, frame5, frame6
+   
+    
+    
+    but.config(font= ("Arial", 10))
+    but1.config(font= ("Arial", 10))
 
-    if 'frame' in globals() and frame is not None:
-        frame.destroy()
-        frame = None
-    if 'frame2' in globals() and frame2 is not None:
-        frame2.destroy()
-        frame2 = None
-    if 'frame3' in globals() and frame3 is not None:
-        frame3.destroy()
-        frame3 = None
+    destroy_frame(frame=frame6)
+    destroy_frame(frame=frame)
+    destroy_frame(frame=frame2)
+    destroy_frame(frame=frame3)
+    destroy_frame(frame=frame4)
+    destroy_frame(frame=frame5)
+    
     
     def aktivni(val):
         global rychlost
@@ -133,20 +140,41 @@ def Semafor():
     potrvzeno.pack(pady=10)
 
 def Texttt():
-
-    global frame, frame2, frame3, x, barva
-
+    import random
+    global frame, frame2, frame3, x, barva, name_label, tmave_pismo, frame4, frame5, frame6
+   
+    names = [
+    "Adéla", "Alena", "Andrea", "Aneta", "Barbora", "Blanka", "Diana", "Dominika", "Eva", "Hana", 
+    "Helena", "Ilona", "Jana", "Karolína", "Kristýna", "Lucie", "Martina", "Monika", "Nikol", 
+    "Petra", "Radka", "Renáta", "Simona", "Tereza", "Veronika", "Zuzana", "Anežka", "Beáta", "Bohuslava", 
+    "Cecílie", "Dana", "Dita", "Ema", "Ester", "Fanny", "Gabriela", "Hana", "Iva", "Ivana", "Jitka", 
+    "Julie", "Karla", "Klára", "Lenka", "Liana", "Lucie", "Marcela", "Michaela", "Milan", "Monika", 
+    "Nina", "Olga", "Pavla", "Renáta", "Růžena", "Sandra", "Silvie", "Tatiana", "Věra", "Viktorie", 
+    "Vladimíra", "Zdenka", "Zlata", "Alice", "Berta", "Blanka", "Božena", "Dita", "Dominika", "Edita", 
+    "Emílie", "Johana", "Julie", "Kamila", "Květoslava", "Ladislava", "Laura", "Ludmila", "Margarita", 
+    "Milada", "Naděžda", "Nikola", "Oľga", "Petra", "Radoslava", "Sára", "Veronika", "Vladislava", 
+    "Xenie", "Yveta", "Zuzana", "Marcela", "Magdalena", "Bohumila", "Kateřina", "Zora", "Marta", 
+    "Ilona", "Gita", "Petra", "Sylvie", "Irena", "Lucie", "Denisa", "Alžběta", "Monika", "Jana", "Radmila",
+    "Milena", "Drahomíra", "Karla", "Lenka", "Adéla", "Kamila", "Mariana", "Michaela", "Tatjana", 
+    "Zuzana", "Lívia", "Tereza", "Ivana", "Věra", "Erika", "Rita", "Renáta", "Veronika", "Martina", 
+    "Dagmar", "Olga", "Irena", "Klára", "Marta", "Marcela", "Monika", "Petra", "Karolína", "Eva"
+]
+    name = None
     
     
-    try:
-        if frame is not None:
-            frame.destroy()
-            frame = None
-        if frame2 is not None:
-            frame2.destroy()
-            frame2 = None
-    except:
-        print("")
+    
+    
+    but.config(font= ("Arial", 10))
+    but1.config(font= ("Arial", 10))
+
+    destroy_frame(frame=frame)
+    destroy_frame(frame=frame2)
+    destroy_frame(frame=frame3)
+    destroy_frame(frame=frame4)
+    destroy_frame(frame=frame5)
+        
+    
+    
     
     if frame3 is not None:  # Ujistíme se, že frame3 zničíme pouze, pokud není None
         frame3.destroy()
@@ -188,40 +216,44 @@ def Texttt():
     canvas.create_oval(205, 50, 305, 150, fill="red", outline="red")  # Pravý kruh
     canvas.create_oval(150, 100, 250, 150, fill="red", outline="red")
     canvas.create_polygon(301,120,104,120, 200, 280,  fill="red" )
-    canvas.create_text(200, 150, text="Jméno", font=("Arial", 20), fill="white")
-
+    name_label = canvas.create_text(200, 150, text= name, font=("Arial", 20), fill="white")
+    tmave_pismo = None
     def Red():
-        global barva
+        global barva, name_label, tmave_pismo
+        tmave_pismo = False
         barva = "červená"
         canvas.create_oval(100, 50, 200, 150, fill="red", outline="red")  # Levý kruh
         canvas.create_oval(205, 50, 305, 150, fill="red", outline="red")  # Pravý kruh
         canvas.create_oval(150, 100, 250, 150, fill="red", outline="red")
         canvas.create_polygon(301,120,104,120, 200, 280,  fill="red" )
-        canvas.create_text(200, 150, text="Jméno", font=("Arial", 20), fill="white")
+        name_label = canvas.create_text(200, 150, text= name, font=("Arial", 20), fill="white")
     def Blue():
-        global barva
+        global barva, name_label, tmave_pismo
+        tmave_pismo = False
         barva = "modrá"
         canvas.create_oval(100, 50, 200, 150, fill="blue", outline="blue")  # Levý kruh
         canvas.create_oval(205, 50, 305, 150, fill="blue", outline="blue")  # Pravý kruh
         canvas.create_oval(150, 100, 250, 150, fill="blue", outline="blue")
         canvas.create_polygon(301,120,104,120, 200, 280,  fill="blue" )
-        canvas.create_text(200, 150, text="Jméno", font=("Arial", 20), fill="white")
+        name_label = canvas.create_text(200, 150, text= name, font=("Arial", 20), fill="white")
     def Pink():
-        global barva
+        global barva, name_label, tmave_pismo
+        tmave_pismo = True
         barva = "růžová"
         canvas.create_oval(100, 50, 200, 150, fill="pink", outline="pink")  # Levý kruh
         canvas.create_oval(205, 50, 305, 150, fill="pink", outline="pink")  # Pravý kruh
         canvas.create_oval(150, 100, 250, 150, fill="pink", outline="pink")
         canvas.create_polygon(301,120,104,120, 200, 280,  fill="pink" )
-        canvas.create_text(200, 150, text="Jméno", font=("Arial", 20), fill="black")
+        name_label = canvas.create_text(200, 150, text= name, font=("Arial", 20), fill="black")
     def Yellow():
-        global barva
+        global barva, name_label, tmave_pismo
+        tmave_pismo = True
         barva = "žlutá"
         canvas.create_oval(100, 50, 200, 150, fill="yellow", outline="yellow")  # Levý kruh
         canvas.create_oval(205, 50, 305, 150, fill="yellow", outline="yellow")  # Pravý kruh
         canvas.create_oval(150, 100, 250, 150, fill="yellow", outline="yellow")
         canvas.create_polygon(301,120,104,120, 200, 280,  fill="yellow")
-        canvas.create_text(200, 150, text="Jméno", font=("Arial", 20), fill="black")
+        name_label = canvas.create_text(200, 150, text= name, font=("Arial", 20), fill="black")
     x = None
     barva = "červená"
 
@@ -233,14 +265,36 @@ def Texttt():
             x = canvas.create_text(200, 170, text=random.choice(vlastnosti), font=("Arial", 15), fill="black")
         else:
             x = canvas.create_text(200, 170, text=random.choice(vlastnosti), font=("Arial", 15), fill="white")
-    frame4 = Frame(okno, bg= "black",  width=340, height=40)
-    frame4.place(x =100, y = 400)
+    frame4 = Frame(okno, bg= "black",  width=360, height=50)
+    frame4.place(x =80, y = 350)
 
-    frame5 = Frame(okno, bg= "black",  width=340, height=90)
+    frame5 = Frame(okno, bg= "black",  width=340, height=150)
     frame5.place(x =100, y = 500)
     flirt = Button(frame5,text="Flirt", fg = "white", bg = "black", relief="sunken", bd = 5, font = ("Arial", 30), command=Flirt)
-    flirt.place(x = 95, y = 0)
+    flirt.place(x = 0, y = 0)
+    
+    def zadej_jmeno():
+        global name_label , tmave_pismo
+        
+        
+        
+        
+        if name_label is not None:
+            canvas.delete(name_label)
 
+        
+
+        
+        name = random.choice(names)
+        
+        if(tmave_pismo):
+            name_label = canvas.create_text(200, 150, text=name, font=("Arial", 20), fill="black")
+        else:
+            name_label = canvas.create_text(200, 150, text=name, font=("Arial", 20), fill="white")
+
+
+    jmeno = Button(frame5,text="Jméno", fg = "white", bg = "black", relief="sunken", bd = 5, font = ("Arial", 30), command=zadej_jmeno)
+    jmeno.place(x = 140, y = 0)
 
     button = Button(frame4,text="Žlutá", fg = "white", bg = "black", relief="sunken", bd = 5, font = (20), command = Yellow)
     button.place(x =0, y = 0)
@@ -250,16 +304,31 @@ def Texttt():
     button2.place(x =160, y = 0)
     button2 = Button(frame4,text="Červená", fg = "white", bg = "black", relief="sunken", bd = 5, font = (20), command = Red)
     button2.place(x =255, y = 0)
+
+
     
+def destroy_frame(frame):
+    if frame is not None:
+        frame.destroy()
+        return None
+    return frame
+
+
 
 
 frame_buttons = Frame(okno, bg="black")  # Vytvoříme frame pro tlačítka
 frame_buttons.pack(side=TOP, padx=10, pady=10)  # Tento frame umístíme nahoře
 
-but = Button(frame_buttons, text="Semafor", command=Semafor, font=('arial', 10), bg = "black", fg = "white")
+but = Button(frame_buttons, text="Semafor", command=Semafor, font=('arial', 30), bg = "black", fg = "white")
 but.pack(side=LEFT, padx=10)  # Tlačítko Semafor
 
-but1 = Button(frame_buttons, text="Srdce", command=Texttt, font=('arial', 10), bg = "black", fg = "white")
+but1 = Button(frame_buttons, text="Srdce", command=Texttt, font=('arial', 30), bg = "black", fg = "white")
 but1.pack(side=LEFT, padx=10)  # Tlačítko Zkouška
 
+frame6 = Frame(okno, bg = "black")
+frame6.pack(side= TOP)
+
+
+info = Label(frame6, text = "Zvolte si aplikaci", font = ("Arial", 30), bg = "black", fg = "white")
+info.pack(side = TOP, anchor="center")
 okno.mainloop()
